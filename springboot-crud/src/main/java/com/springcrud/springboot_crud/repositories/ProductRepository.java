c3e5596093a2e60b3a2f79f5efe4fced820e07ab
TheNotoriousPhilipDev <144860448+TheNotoriousPhilipDev@users.noreply.github.com>
2024-05-30 10:12:15 -0500

    Delete springboot-crud directory

--- springboot-crud/src/main/java/com/springcrud/springboot_crud/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springcrud.springboot_crud.repositories;
-
-import com.springcrud.springboot_crud.entities.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-
-}
